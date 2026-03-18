202bf373ff834219fb5091b8a2c286ded829b263
naveenraj272005 <naveenraj272005@gmail.com>
2026-03-18 11:48:59 +0530

    Delete secure-file-sharing directory

--- secure-file-sharing/backend/lambda.js
+++
@@ -1,8 +0,0 @@
-// lambda.js
-// AWS Lambda handler — wraps the Express app using serverless-http
-// Used when deploying to Lambda + API Gateway
-
-const serverless = require("serverless-http");
-const app = require("./server");
-
-module.exports.handler = serverless(app);
